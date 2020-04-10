9e37be90e1e69aafabbaf7ba4257b47ad2da70aa
CriminalBacon <michael.tate@gmail.com>
2020-04-10 11:40:06 +0100

    added click and drag functionality

--- js/script.js
+++ js/script.js
@@ -7,9 +7,27 @@ function createDivs(size){
         for (var i = 0; i < size; i++){
             var div = document.createElement("div");
             div.className = "gridBlock";
+            div.addEventListener("mousedown", changeIsMouseDown);
+            div.addEventListener("mouseover", changeBackground);
+            div.addEventListener("mouseup", changeIsMouseDown);
+            div.addEventListener("drag", changeIsMouseDown);
             document.getElementById("main-div").appendChild(div);
         }
     }
 }
 
-createDivs(16);
+function changeIsMouseDown(){
+
+    isMouseDown = !isMouseDown;
+
+}
+
+function changeBackground(){
+    if (isMouseDown){
+        this.style.backgroundColor = "black";
+    }
+}
+
+var isMouseDown = false;
+
+createDivs(64);
